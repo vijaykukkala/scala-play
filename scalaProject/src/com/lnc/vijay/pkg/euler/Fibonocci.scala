ef9c0c3b873f16c1cee098ed4208e764807bffbb
vijay kukkala <vijay.kukkala@gmail.com>
2012-05-19 21:35:40 -0500

    Second Euler problem

---
+++ scalaProject/src/com/lnc/vijay/pkg/euler/Fibonocci.scala
@@ -0,0 +1,34 @@
+package com.lnc.vijay.pkg.euler
+import scala.collection.mutable.HashMap
+import scala.collection.mutable.LinkedList
+
+/* Calculate the sum of all even fibonacci numbers bellow 4 million */
+object Fibonocci {
+
+  val fMap = new HashMap[Int, Long] // used to store the Fibonocci already calcuated.
+
+  val maxValue: Long = 4000000
+
+  //Initialize
+  fMap += 1 -> 1;
+  fMap += 2 -> 2;
+
+  def main(args: Array[String]) {
+    var fVal = 0L; var i = 1;
+    while (fVal >= 0) {
+      fVal = f(i)
+      i += 1
+    }
+    println(fMap.values.filter(_ % 2 == 0).reduceRight(_ + _))
+  }
+
+  def f(x: Int): Long = {
+    if (!fMap.contains(x)) {
+      val result = f(x - 1) + f(x - 2)
+      if (result < maxValue)
+        fMap += x -> (result)
+      else return -1
+    }
+    return fMap(x)
+  }
+}
