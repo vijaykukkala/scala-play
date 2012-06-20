f3b5c548bb471a7ef1ac96bdbeb2094da53cfd9a
vijaykukkala <jarya2879>
2012-06-19 22:32:41 -0500

    Problem 10

---
+++ src/com/lnc/vijay/pkg/euler/SumPrimes.scala
@@ -0,0 +1,20 @@
+package com.lnc.vijay.pkg.euler
+import scala.collection.immutable.Range
+import scala.collection.mutable.LinkedList
+import scala.collection.mutable.MutableList
+
+/**
+ * http://projecteuler.net/problem=10
+ * 
+ * The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+ * Find the sum of all the primes below two million.
+ */
+object SumPrimes extends PrimeNumber{
+
+  def main(args: Array[String]) {
+	  var primesList = MutableList[Long]();
+	  new Range(2, 2000000, 1).foreach(arg => { if (isPrime(arg)) primesList += arg })
+	  println(primesList.reduceLeft(_+_)) // reduceRight throws a Exception
+	  
+  }
+}
