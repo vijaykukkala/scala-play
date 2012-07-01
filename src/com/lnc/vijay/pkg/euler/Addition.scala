11d2857c55973b34f6cd36a4ab4d85c7e2ac0c96
vijaykukkala <jarya2879>
2012-06-30 23:25:19 -0500

    Problem 20

--- src/com/lnc/vijay/pkg/euler/Addition.scala
+++ src/com/lnc/vijay/pkg/euler/Addition.scala
@@ -16,4 +16,21 @@ class Addition {
     val result = ("" /: zf)(_.toString +""+_.toString).reverse
     result
   }
+  
+  /**
+   * http://projecteuler.net/problem=20
+   * 
+   * n! means n  (n  1)  ...  3  2  1
+   * 
+   * For example, 10! = 10  9  ...  3  2  1 = 3628800,
+   * and the sum of the digits in the number 10! is 3 + 6 + 2 + 8 + 8 + 0 + 0 = 27.
+   * 
+   * Find the sum of the digits in the number 100!
+   */
+  def multiply(xs:String, m:Int): String = {
+     val x = xs.grouped(1).toList
+     val y = x.map(_.toInt).reverse.map{var v, k, j, temp = 0; z=> 
+       							{temp = (z * m) + v; k = temp/10; j = temp % 10; v = k; j}}
+    ("" /: y.reverse)(_+""+_)   
+  }
 }
---
+++ src/com/lnc/vijay/pkg/euler/MultiplicationSpec.scala
@@ -0,0 +1,51 @@
+package com.lnc.vijay.pkg.euler
+
+import org.junit.runner.RunWith
+import org.scalatest.Style
+import org.scalatest.FlatSpec
+import org.scalatest.GivenWhenThen
+import org.scalatest.junit.JUnitRunner
+import scala.io.Source
+import java.io.File
+
+@RunWith(classOf[JUnitRunner])
+class MultiplicationSpec extends FlatSpec with GivenWhenThen {
+
+  def operation = new Addition
+  
+  "Multiply Operation" must "multiply two numbers" in {
+    val result = operation.multiply("0026", 7)
+    assert("0182" equals result)
+  }
+
+  it must "multiply two numbers 26 and 27 " in {
+    val result = operation.multiply("0026", 27)
+    assert(26*27 == result.toInt)
+  }
+  
+  it must "multiply two numbers 262727262724323424 and 4 " in {
+    val result = operation.multiply("00262727262724323424", 4)
+    assert("01050909050897293696" equals result)
+  }
+  
+  it must "find Factorial of numbers upto 10" in {
+    var result =  "01"
+    for(i <- 1 to 10) {
+      result = operation.multiply("0" + result, i)
+    }
+    println(result)
+    var expected = (1 /: List.range(1, 11))(_*_)
+    assert(expected == result.toInt)
+  }
+  
+  it must "find Factorial of numbers upto 100" in {
+    var result =  "01"
+    for(i <- 1 to 100) {
+      result = operation.multiply("00" + result, i)      
+    }
+    println(result)
+    println("sum of digits:" + result.grouped(1).toList.map(_.toInt).sum)
+    
+  }
+  
+}
