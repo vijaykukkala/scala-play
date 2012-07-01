1873e7cb9207e75f126a51846819bd36c2f8dfea
vijaykukkala <jarya2879>
2012-07-01 11:51:10 -0500

    refactored to use Sieve_of_Eratosthenes

--- src/com/lnc/vijay/pkg/euler/SumPrimes.scala
+++ src/com/lnc/vijay/pkg/euler/SumPrimes.scala
@@ -7,13 +7,26 @@ import scala.collection.mutable.MutableList
  * 
  * The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
  * Find the sum of all the primes below two million.
+ * Refactored to use the http://en.wikipedia.org/wiki/Sieve_of_Eratosthenes
+ * increased the jvm memory size
  */
 object SumPrimes extends PrimeNumber{
 
   def main(args: Array[String]) {
-	  val primesList = for (i <- List.range(2, 2000000) if (isPrime(i))) yield i
-	  println( (0 /: primesList)(_+_)) // reduceRight throws a Exception in thread "main" java.lang.StackOverflowError
-	  									// some explanation is given here http://days2012.scala-lang.org/sites/days2012/files/bjarnason_trampolines.pdf
+      
+      val rangeLimit = 2000000
+	  var filtered = List.range(2, rangeLimit, 1)
 	  
-  }
+	  var sieve = filtered.head
+	  var resultList = List[Int]()
+	  
+	  while(sieve <= Math.sqrt(rangeLimit)) {
+	    resultList ::= sieve
+	    filtered = filtered.filter(_% sieve != 0)
+	    sieve = filtered.head	    
+	  }
+	  val sumResultList = (0L /: resultList) (_+_)
+	  val sumFiltered = (0L /: filtered) (_+_)
+	  println("Final Total: %d".format(sumFiltered + sumResultList))
+  } 
 }
