eb5a0aabef28b7b176c93dac870420f880ab178b
vijaykukkala <jarya2879>
2012-06-30 21:32:40 -0500

    added comments

--- src/com/lnc/vijay/pkg/euler/Addition.scala
+++ src/com/lnc/vijay/pkg/euler/Addition.scala
@@ -1,21 +1,19 @@
 package com.lnc.vijay.pkg.euler
 
+/**
+ * http://projecteuler.net/problem=13
+ * 
+ * Work out the first ten digits of the sum of the following one-hundred 50-digit numbers
+ */
 class Addition {
 
   def add(xs:String, ys:String):String = {
-//    println("xs:" + xs + " ys:" +ys)
     val y = ys.grouped(1).toList.map(_.toInt)
     val x = xs.grouped(1).toList.map(_.toInt)
     val z = x zip y
-//    println(z)
     val z1=z.map{case(a,b) => a+b}
-//    println("z1=" +z1)
-    val zr = z1.reverse
-//    println("zr=" + zr)
-    val zf =zr.map{var v,k,j= 0; x => {k = (x+v)/10; j =(x + v)%10; v=k; j}}
-//    println("zf=" + zf)
+    val zf =z1.reverse.map{var v,k,j= 0; x => {k = (x+v)/10; j =(x + v)%10; v=k; j}}
     val result = ("" /: zf)(_.toString +""+_.toString).reverse
-//    println(result)
     result
   }
 }
