2d8ca383538ec763aa10996b138471e7a4034565
vijaykukkala <jarya2879>
2012-07-11 20:05:25 -0500

    refactored

--- src/com/lnc/vijay/pkg/euler/Euler1.scala
+++ src/com/lnc/vijay/pkg/euler/Euler1.scala
@@ -3,14 +3,9 @@ package com.lnc.vijay.pkg.euler
 object Euler1 {
 
     def main(args: Array[String]){
-
-    val f = for (i <- List.range(1, 1000) if (factor(i, 3) || factor(i, 5))) yield i
-    val sum = (0 /: f) (_+_)
-    println(sum)
-      
+	    val f = for (i <- List.range(1, 1000) if ((i % 3 == 0) || (i % 5 == 0))) yield i
+	    val sum = (0 /: f) (_+_)
+	    println(sum)   
     }
     
-    def factor(x: Int, y: Int): Boolean = {
-     (x / y) * y == x
-    }
 }
