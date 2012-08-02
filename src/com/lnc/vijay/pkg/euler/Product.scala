c34c21f84159d10f071cae50a2e4e02d9aa2ab61
vijaykukkala <jarya2879>
2012-08-01 21:23:30 -0500

    problem 28

--- src/com/lnc/vijay/pkg/euler/Product.scala
+++ src/com/lnc/vijay/pkg/euler/Product.scala
@@ -51,6 +51,26 @@ object Product {
   }
   def square(x: Int): Int = { x * x }
   
+ 
+  /**
+   * http://projecteuler.net/problem=28
+   * Starting with the number 1 and moving to the right in a clockwise direction a 5 by 5 spiral is formed as follows:
+   * 			21 22 23 24 25
+   * 			20  7  8  9 10
+   * 			19  6  1  2 11
+   * 			18  5  4  3 12
+   * 			17 16 15 14 13
+   * It can be verified that the sum of the numbers on the diagonals is 101.
+   * What is the sum of the numbers on the diagonals in a 1001 by 1001 spiral formed in the same way?
+   */
+  def spiralSum() {
+    def f(x:Int) = 4 * square(x) - 6 * x + 6
+    
+    val fx= for(x <- 3 to 1001 by 2) yield f(x) 
+    
+    val sumFx = (1 /: fx)(_+_)
+    
+  }
   
   /**
    * http://projecteuler.net/problem=4
